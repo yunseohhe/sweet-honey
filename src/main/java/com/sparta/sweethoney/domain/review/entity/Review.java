55b2f92490fe759b67a2922f0acd9d15eb38c065
chukye1216 <chukye1216@gmail.com>
2024-09-19 22:33:13 +0900

    [add] review 초기 세팅 완료

---
+++ src/main/java/com/sparta/sweethoney/domain/review/entity/Review.java
@@ -0,0 +1,31 @@
+package com.sparta.sweethoney.domain.review.entity;
+
+import com.sparta.sweethoney.domain.store.entity.Store;
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Getter
+@NoArgsConstructor
+@Table(name = "review")
+public class Review {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long reviewId;
+
+    @Column(nullable = false)
+    private Integer rating;
+
+    @Column(nullable = false, length = 200)
+    private String comment;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "store_id", nullable = false)
+    private Store store;
+
+    @OneToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "order_id", nullable = false)
+    private Order order;
+
+}
---
+++ src/main/java/com/sparta/sweethoney/domain/review/service/ReviewService.java
@@ -0,0 +1,13 @@
+package com.sparta.sweethoney.domain.review.service;
+
+import com.sparta.sweethoney.domain.review.repository.ReviewRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Service
+@RequiredArgsConstructor
+@Transactional
+public class ReviewService {
+    private final ReviewRepository reviewRepository;
+}
