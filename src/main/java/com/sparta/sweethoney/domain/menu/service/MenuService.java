3ade270dd6628a63383fae928bdbe328db4f2075
ican0422 <ican0422@gmail.com>
2024-09-20 00:21:51 +0900

    [refactor] : 중복 코드 메서드화

--- src/main/java/com/sparta/sweethoney/domain/menu/service/MenuService.java
+++ src/main/java/com/sparta/sweethoney/domain/menu/service/MenuService.java
@@ -12,6 +12,8 @@ import jakarta.transaction.Transactional;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
+import java.util.Optional;
+
 @Service
 @RequiredArgsConstructor
 public class MenuService {
@@ -22,12 +24,10 @@ public class MenuService {
     @Transactional
     public PutMenuResponseDto updateMenu(Long storeId, Long menuId, PutMenuRequestDto requestDto) {
         // 가게 조회
-        Store store = storeRepository.findById(storeId).orElseThrow(() ->
-                new IllegalArgumentException("해당 가게가 없습니다."));
+        Store store = findStoreOrElseThrow(storeId);
 
         // 메뉴 조회
-        Menu menu = menuRepository.findByIdAndStoreId(menuId, store.getId()).orElseThrow(() ->
-                new IllegalArgumentException("해당 메뉴가 없습니다."));
+        Menu menu = findMenuOrElseThrow(menuId, store.getId());
 
         // 메뉴 수정
         menu.update(requestDto);
@@ -41,12 +41,10 @@ public class MenuService {
     @Transactional
     public DeleteMenuResponseDto deleteMenu(Long storeId, Long menuId) {
         // 가게 조회
-        Store store = storeRepository.findById(storeId).orElseThrow(() ->
-                new IllegalArgumentException("해당 가게가 없습니다."));
+        Store store = findStoreOrElseThrow(storeId);
 
         // 메뉴 조회
-        Menu menu = menuRepository.findByIdAndStoreId(menuId, store.getId()).orElseThrow(() ->
-                new IllegalArgumentException("해당 메뉴가 없습니다."));
+        Menu menu = findMenuOrElseThrow(menuId, store.getId());
 
         // 미판매인지 확인
         if (menu.getStatus() == MenuStatus.INACTIVE) {
@@ -60,5 +58,17 @@ public class MenuService {
         DeleteMenuResponseDto responseDto = new DeleteMenuResponseDto(menu);
 
         return responseDto;
+    }
+
+    /* 가게 조회 */
+    private Store findStoreOrElseThrow(Long storeId) {
+        return storeRepository.findById(storeId).orElseThrow(() ->
+                new IllegalArgumentException("해당 가게가 없습니다."));
+    }
+
+    /* 메뉴 조회 */
+    private Menu findMenuOrElseThrow(Long menuId, Long storeId) {
+        return menuRepository.findByIdAndStoreId(menuId, storeId).orElseThrow(() ->
+                new IllegalArgumentException("해당 메뉴가 없습니다."));
     }
 }
