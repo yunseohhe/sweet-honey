daaa881939f056dbe264dbfe17b34da006bbd3d3
yunseohhe <dbstj4566@naver.com>
2024-09-19 11:43:34 +0900

    init

---
+++ src/main/java/com/sparta/sweethoney/SweetHoneyApplication.java
@@ -0,0 +1,13 @@
+package com.sparta.sweethoney;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SweetHoneyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SweetHoneyApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/sparta/sweethoney/SweetHoneyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sparta.sweethoney;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SweetHoneyApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
