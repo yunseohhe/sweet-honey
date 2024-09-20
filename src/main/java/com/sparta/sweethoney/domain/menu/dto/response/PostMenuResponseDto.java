5d773fa98ed18c491e7662113f3e39c235ad6d86
ican0422 <ican0422@gmail.com>
2024-09-20 09:44:26 +0900

    [update] : 메뉴 생성 임시 커밋

---
+++ src/main/java/com/sparta/sweethoney/domain/menu/dto/response/PostMenuResponseDto.java
@@ -0,0 +1,12 @@
+package com.sparta.sweethoney.domain.menu.dto.response;
+
+import com.sparta.sweethoney.domain.menu.entity.MenuStatus;
+import lombok.Getter;
+
+@Getter
+public class PostMenuResponseDto {
+    private Long id;
+    private String name;
+    private int price;
+    private MenuStatus status;
+}
