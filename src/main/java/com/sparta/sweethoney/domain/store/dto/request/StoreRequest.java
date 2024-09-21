14ebc118855961ffd2dee05b6d4369bad5aa9601
yunseohhe <dbstj4566@naver.com>
2024-09-21 16:09:11 +0900

    [refactor] store 리팩토링

--- src/main/java/com/sparta/sweethoney/domain/store/dto/request/StoreRequest.java
+++ src/main/java/com/sparta/sweethoney/domain/store/dto/request/StoreRequest.java
@@ -1,8 +1,10 @@
 package com.sparta.sweethoney.domain.store.dto.request;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+import org.springframework.format.annotation.DateTimeFormat;
 
 import java.time.LocalTime;
 
@@ -11,7 +13,12 @@ import java.time.LocalTime;
 @AllArgsConstructor
 public class StoreRequest {
     private String name;
+
+    @JsonFormat(pattern = "HH:mm")
     private LocalTime openTime;
+
+    @JsonFormat(pattern = "HH:mm")
     private LocalTime closeTime;
+
     private int minOrderPrice;
 }
